fc160ef56b246dc4574f3a7e4bc79819902344f8
Giacomo <giacomo.1936@gmail.com>
2021-01-19 18:02:06 +0100

    aggiunto file db dresses e modifica env

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Dress;
+
 class HomeController extends Controller
 
 {
     public function index() {
-        return 'test';
+        $all_dresses = Dress::all();
+        dd($all_dresses);
+
+        $data = [
+            'prova' => 'ueee'
+        ];
+
+        return view('home', $data);
     }
 }
